09c48f2dd9108e69d8b4464cb2e14a08eb300081
Manuel Dautt B <mdautt890@gmail.com>
2021-11-04 18:42:54 -0500

    Ajustes varios add finc

--- var_ambiente.sh
+++ var_ambiente.sh
@@ -6,17 +6,22 @@ do
   if [[ "$nombre" == 'RED_NAME' ]]; then
     echo la variable "$nombre" tiene asignado el valor "$valor"
     sed -i -e 's/'RED_DUMMY'/'"$valor"'/g' ./docker-compose.yaml
-    echo Se realizo el cambio en el docker-compose
+    echo Se realizo el cambio en el docker-compose la red dummy
   fi
   if [[ "$nombre" == 'PHP_CONT_PORT' ]]; then
     echo la variable "$nombre" tiene asignado el valor "$valor"
     sed -i -e 's/'php:9000'/'php:"$valor"'/g' ./supports/nginx/default.conf
-    echo Se realizo el cambio en el nginx default.conf
+    echo Se realizo el cambio en el nginx default.conf el php port
   fi
   if [[ "$nombre" == 'NGINX_CONT_PORT' ]]; then
     echo la variable "$nombre" tiene asignado el valor "$valor"
     sed -i -e 's/listen 80/listen '"$valor"'/g' ./supports/nginx/default.conf
-    echo Se realizo el cambio en el nginx default.conf
+    echo Se realizo el cambio en el nginx default.conf el listen port
   fi
+  if [[ "$nombre" == 'PHP_CONT_PORT' ]]; then
+    echo la variable "$nombre" tiene asignado el valor "$valor"
+    sed -i -e 's/EXPOSE 9000/EXPOSE '"$valor"'/g' ./Dockerfile
+    echo Se realizo el cambio en el Dockerfile el EXPOSE port
+  fi  
 done < .env
 echo Fin del Proceso

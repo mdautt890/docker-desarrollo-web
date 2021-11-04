c150e53e3dfb130e611998b20131fdec3c3598a7
Manuel Dautt <mdautt890@gmail.com>
2021-11-04 13:18:21 -0500

    Update var_ambiente.sh

--- var_ambiente.sh
+++ var_ambiente.sh
@@ -8,5 +8,15 @@ do
     sed -i -e 's/'RED_DUMMY'/'"$valor"'/g' ./docker-compose.yaml
     echo Se realizo el cambio en el docker-compose
   fi
+  if [[ "$nombre" == 'PHP_CONT_PORT' ]]; then
+    echo la variable "$nombre" tiene asignado el valor "$valor"
+    sed -i -e 's/'php:9000'/'php:"$valor"'/g' ./supports/nginx/default.conf
+    echo Se realizo el cambio en el nginx default.conf
+  fi
+  if [[ "$nombre" == 'NGINX_CONT_PORT' ]]; then
+    echo la variable "$nombre" tiene asignado el valor "$valor"
+    sed -i -e 's/'listen 80'/'listen "$valor"'/g' ./supports/nginx/default.conf
+    echo Se realizo el cambio en el nginx default.conf
+  fi
 done < .env
 echo Fin del Proceso

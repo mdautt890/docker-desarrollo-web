e79e5bf2eed7cc8d50804178e20c27354767ff68
Manuel Dautt B <mdautt890@gmail.com>
2021-11-04 10:54:40 -0500

    Incluimos mariadb

--- folder_struct.sh
+++ folder_struct.sh
@@ -5,12 +5,17 @@ mkdir ./src
 cd supports
 mkdir mysql
 mkdir postgres
+mkdir mariadb
 cd postgres
 mkdir dbdata
 cd ..
 cd mysql
 mkdir entrypoint
 mkdir data
+cd ..
+cd mariadb
+mkdir config
+mkdir dbdata
 cd ..
 cd ..
 echo "Done..."
